b795de81ce0a3cc9e25ca72da2bd1369b47eb98f
Pedro Chaves <pedroa.chaves10@gmail.com>
2024-05-07 10:41:33 -0300

    Create aula15.py
    practicing the correct use of the input function.

---
+++ classes/aula15.py
@@ -0,0 +1,13 @@
+#Utilizando a função input da maneira correta
+
+nome=input('Digite o seu nome: ')
+print(f'Seja bem vindo {nome} ')
+
+idade=input('Digite a sua idade: ')
+int_idade=int(idade)
+ano_de_nascimento = 2024 - int_idade
+
+
+
+print(f'O seu nome é {nome}, a sua idade é {idade}, e nasceu no ano de {ano_de_nascimento}'
+      )
