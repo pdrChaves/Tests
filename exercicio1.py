4931e245d679f738b753d0aba3022fdaf8c07b95
Pedro Chaves <pedroa.chaves10@gmail.com>
2024-05-07 10:04:11 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,34 @@
+nome=input('Qual seu nome? ')
+sobrenome=input('Qual seu sobrenome? ')
+idade=int(input('Quantos anos você tem? '))
+ano_de_nascimento= 2024 - idade
+maior_de_idade= idade >= 18 #PODE DAR ERRADO AS VEZES
+altura_em_metros=(input('Qual sua altura? ')
+                  )
+print('Dados pessoais:')
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros', altura_em_metros)
+nome=input('digite seu nome')
+dez_vezes_nome= nome * 10
+idade=int(input('digite sua idade'))
+idade_dividido_dois= int(idade) / 2
+maior_de_idade= idade >= 18 
+print('maior de idade:' , maior_de_idade)
+print('idade dividido por dois:' , idade_dividido_dois)
+
+n1=int(input('digite um numero'))
+n2=int(input('digite outro numero'))
+divisao= n1 / n2
+divisao_inteiro= n1 // n2
+multiplicação= n1 * n2
+adição= n1 + n2
+subtração= n1 / n2
+print('divisão:' , divisao)
+print('multiplicação' , multiplicação)
+print('subtração' , subtração)
+print('adição' , adição)
+
