a5059f578ce8b4210826e63e2c954e68244114e7
Pedro Chaves <pedroa.chaves10@gmail.com>
2024-05-13 21:56:04 -0300

    Create test_3.py

---
+++ test_3.py
@@ -0,0 +1,9 @@
+# Operadores Matemáticos 
+
+n1=int(input('digite um numero'))
+n2=int(input('digite outro numero'))
+divisao= n1 / n2
+divisao_inteiro= n1 // n2
+multiplicação= n1 * n2
+adição= n1 + n2
+subtração= n1 / n2
