0bd9e7b3d2485e62d232c6d5a81102ba911721ce
Pedro Chaves <pedroa.chaves10@gmail.com>
2024-05-13 21:54:24 -0300

    Create test_2.py

---
+++ test_2.py
@@ -0,0 +1,30 @@
+# Coleta de dados:
+
+nome=input('Qual seu nome? ')
+sobrenome=input('Qual seu sobrenome? ')
+idade=int(input('Quantos anos você tem? '))
+ano_de_nascimento= 2024 - idade
+maior_de_idade= idade >= 18 #PODE DAR ERRADO AS VEZES
+altura_em_metros=(input('Qual sua altura? ')
+                  )
+print('Dados pessoais:')
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros', altura_em_metros)
+nome=input('digite seu nome')
+dez_vezes_nome= nome * 10
+idade=int(input('digite sua idade'))
+idade_dividido_dois= int(idade) / 2
+maior_de_idade= idade >= 18 
+print('maior de idade:' , maior_de_idade)
+print('idade dividido por dois:' , idade_dividido_dois)
+
+
+print('divisão:' , divisao)
+print('multiplicação' , multiplicação)
+print('subtração' , subtração)
+print('adição' , adição)
+
