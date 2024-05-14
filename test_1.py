5e224019aeed3808f7868dbbdc3fb3172ed12449
Pedro Chaves <pedroa.chaves10@gmail.com>
2024-05-13 21:51:45 -0300

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,21 @@
+# Banco de dados e teste com operadores matemáticos
+
+nome=input('digite seu nome')
+dez_vezes_nome= nome * 10
+idade=int(input('digite sua idade'))
+idade_dividido_dois= int(idade) / 2
+maior_de_idade= idade >= 18 
+print('maior de idade:' , maior_de_idade)
+print('idade dividido por dois:' , idade_dividido_dois)
+
+n1=int(input('digite um numero'))
+n2=int(input('digite outro numero'))
+divisao= n1 / n2
+multiplicação= n1 * n2
+adição= n1 + n2
+subtração= n1 / n2
+print('divisão:' , divisao)
+print('multiplicação' , multiplicação)
+print('subtração' , subtração)
+print('adição' , adição)
+
